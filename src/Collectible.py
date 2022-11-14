ad2e3db262d0494c16d51a3fb75f009fd8c8c379
Amit Kumar Shaw <amit.shaw@rwth-aachen.de>
2022-11-14 22:05:22 +0100

    add collectible sprite

---
+++ src/Collectible.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Collectible(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        t1 = pygame.image.load("./assets/images/coin/1.png").convert_alpha()
+        t2 = pygame.image.load("./assets/images/coin/2.png").convert_alpha()
+        t3 = pygame.image.load("./assets/images/coin/3.png").convert_alpha()
+        t4 = pygame.image.load("./assets/images/coin/4.png").convert_alpha()
+
+        self.frames = [pygame.transform.rotozoom(t1, 0, 0.5), pygame.transform.rotozoom(t2, 0, 0.5), pygame.transform.rotozoom(t3, 0, 0.5), pygame.transform.rotozoom(t4, 0, 0.5)]
+        self.animation_index = 0
+
+        self.image = self.frames[self.animation_index]
+
+        self.rect = self.image.get_rect(midtop=pos)
+
+    def animate(self):
+
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames): self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
