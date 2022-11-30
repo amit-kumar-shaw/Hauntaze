9234b95089f53c421fad4333f15fd8f36f94ec0a
Amit Kumar Shaw <amit.shaw@rwth-aachen.de>
2022-11-30 09:25:46 +0100

    implement player coin and enemy collision music

--- src/music.py
+++ src/music.py
@@ -11,3 +11,21 @@ class GameSound(object):
     @staticmethod
     def playbackgroundmusic():
         pygame.mixer.music.play(-1)
+
+
+class PlayerSound(object):
+
+    def __init__(self):
+        pygame.mixer.init()
+        # pygame.mixer.music.load('./assets/Audio/Magic Escape Room.mp3')
+        # pygame.mixer.music.set_volume(0.5)
+
+    def play_enemy_collision(self):
+        pygame.mixer.music.load('./assets/Audio/enemy_collision.mp3')
+        pygame.mixer.music.set_volume(0.7)
+        pygame.mixer.music.play()
+
+    def play_coin_collection(self):
+        pygame.mixer.music.load('./assets/Audio/collect_coins.mp3')
+        pygame.mixer.music.set_volume(0.7)
+        pygame.mixer.music.play()
