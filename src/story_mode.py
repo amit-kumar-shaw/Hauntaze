395fc402c37717c5f2c667c8b3f678546756defb
Amit Kumar Shaw <amit.shaw@rwth-aachen.de>
2022-12-01 22:44:48 +0100

    add game mode file

---
+++ src/story_mode.py
@@ -0,0 +1,10 @@
+import pygame
+from settings import *
+
+
+class StoryMode:
+    def __init__(self):
+        self.status = None
+
+    def run(self):
+        pass
---
+++ src/survival_mode.py
@@ -0,0 +1,10 @@
+import pygame
+from settings import *
+
+
+class SurvivalMode:
+    def __init__(self):
+        self.status = None
+
+    def run(self):
+        pass
