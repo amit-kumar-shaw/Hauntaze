abe67767193f3dfc1314c6a8aef1be3b828802db
Amit Kumar Shaw <amit.shaw@rwth-aachen.de>
2022-12-13 23:05:48 +0100

    ass new enemy: slime

--- src/enemy.py
+++ src/enemy.py
@@ -11,18 +11,10 @@ class Enemy(pygame.sprite.Sprite):
 
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
-        # self.image = pygame.image.load("./assets/images/enemy/tile_0120.png").convert_alpha()
 
-        # t1 = pygame.image.load("./assets/images/enemy/bat/1.png").convert_alpha()
-        # t2 = pygame.image.load("./assets/images/enemy/bat/2.png").convert_alpha()
-        # t3 = pygame.image.load("./assets/images/enemy/bat/3.png").convert_alpha()
-        # t4 = pygame.image.load("./assets/images/enemy/bat/4.png").convert_alpha()
-        #
-        # self.frames = [pygame.transform.rotozoom(t1, 0, 0.75),
-        #                pygame.transform.rotozoom(t2, 0, 0.75),
-        #                pygame.transform.rotozoom(t3, 0, 0.75), pygame.transform.rotozoom(t4, 0, 0.75)]
+        self.type = random.choice(['bat','slime'])
 
-        self.frames = import_frames("./assets/images/enemy/bat", scale=0.75)
+        self.frames = import_frames(f"./assets/images/enemy/{self.type}", scale=0.75)
 
         self.animation_index = random.choice([0, 1, 2, 3])
 
@@ -82,6 +74,8 @@ class Enemy(pygame.sprite.Sprite):
         self.animation_index += 0.1
         if self.animation_index >= len(self.frames): self.animation_index = 0
         self.image = self.frames[int(self.animation_index)]
+        if self.direction.x < 0:
+            self.image = pygame.transform.flip(self.image, True, False)
 
     def update(self):
 
