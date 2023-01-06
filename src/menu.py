b9170218b221edb1ca0bd06796e61831e73b1b30
Amit Kumar Shaw <amit.shaw@rwth-aachen.de>
2023-01-06 16:52:09 +0100

    update alternate menu

--- src/menu.py
+++ src/menu.py
@@ -4,6 +4,7 @@ import pygame
 from settings import *
 from music import GameSound
 
+
 class Menu():
     def __init__(self, ):
         self.screen = pygame.display.get_surface()
@@ -24,11 +25,29 @@ class Menu():
         self.mode1_frames = self.mode_frames('Story Mode')
         self.mode2_frames = self.mode_frames('Survival Mode')
         self.coin_frames = self.coin_text()
-        self.start_surf = self.plain_text('Insert Coin to start', 22)
+        self.start_surf = self.plain_text('Insert Coin to start', 16)
         self.start_rect = self.start_surf.get_rect(center=(SCREEN_WIDTH * 0.5, SCREEN_HEIGHT - 20))
 
         self.sound = GameSound()
         self.sound.menu.play(loops=-1)
+
+        self.story_frames = self.create_frames('Story Mode >')
+        self.survival_frames = self.create_frames('< Survival Mode')
+        self.single_frames = self.create_frames('Single Player >')
+        self.multi_frames = self.create_frames('< Multiplayer')
+        self.vertical_transition = False
+        self.horizontal_transition = False
+        self.player_selected = True
+        self.story = True
+        self.horizontal_index = 5
+        self.vertical_index = 5
+
+        size = 16
+        self.info_msg = []
+        self.info_msg.append(self.text('Lift the curse of the forbidden treasure', size))
+        self.info_msg.append(self.text('Play together to lift the curse of the forbidden treasure', size))
+        self.info_msg.append(self.text('How many levels can you survive?', size))
+        self.info_msg.append(self.text('Compete against each other', size))
 
     def check_input(self):
         keys = pygame.key.get_pressed()
@@ -78,34 +97,36 @@ class Menu():
 
     def update(self):
 
-        self.check_input()
+
 
         self.screen.blit(self.background, (0, 0))
 
+        # self.check_input()
+        # # title animation
+        # self.animation_index += 0.08  # * self.animation_direction
+        # if self.animation_index >= 2: self.animation_index = 0
+        #
+        # self.screen.blit(self.player1_surf, self.player1_rect)
+        #
+        # self.screen.blit(self.player2_surf, self.player2_rect)
+        #
+        # # player 1 insert coin
+        # frame = self.coin_frames[int(self.animation_index)] if not self.is_player1_ready else self.coin_frames[2]
+        # frame_rect = frame.get_rect(center=(SCREEN_WIDTH * 0.25, SCREEN_HEIGHT - 200))
+        # self.screen.blit(frame, frame_rect)
+        #
+        # # player 2 insert coin
+        # frame = self.coin_frames[int(self.animation_index)] if not self.is_player2_ready else self.coin_frames[2]
+        # frame_rect = frame.get_rect(center=(SCREEN_WIDTH * 0.75, SCREEN_HEIGHT - 200))
+        # self.screen.blit(frame, frame_rect)
+        #
+        # if self.is_player1_ready or self.is_player2_ready:
+        #     self.screen.blit(self.start_surf, self.start_rect)
+        #
+        # self.game_mode()
 
-        # title animation
-        self.animation_index += 0.08# * self.animation_direction
-        if self.animation_index >= 2: self.animation_index = 0
-
-        self.screen.blit(self.player1_surf, self.player1_rect)
-
-        self.screen.blit(self.player2_surf, self.player2_rect)
-
-        # player 1 insert coin
-        frame = self.coin_frames[int(self.animation_index)] if not self.is_player1_ready else self.coin_frames[2]
-        frame_rect = frame.get_rect(center=(SCREEN_WIDTH * 0.25, SCREEN_HEIGHT - 200))
-        self.screen.blit(frame, frame_rect)
-
-        # player 2 insert coin
-        frame = self.coin_frames[int(self.animation_index)] if not self.is_player2_ready else self.coin_frames[2]
-        frame_rect = frame.get_rect(center=(SCREEN_WIDTH * 0.75, SCREEN_HEIGHT - 200))
-        self.screen.blit(frame, frame_rect)
-
-        if self.is_player1_ready or self.is_player2_ready:
-            # self.screen.blit(start_msg, msg_rect)
-            self.screen.blit(self.start_surf, self.start_rect)
-
-        self.game_mode()
+        self.update_menu()
+        self.screen.blit(self.start_surf, self.start_rect)
 
     def mode_frames(self, text_msg):
         frames = []
@@ -162,3 +183,133 @@ class Menu():
             frames.append(text_surf)
 
         return frames
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_DOWN]:
+            if not self.horizontal_transition and self.player_selected:
+                self.sound.play_mode_select()
+                self.player_selected = False
+                self.vertical_transition = True
+                self.vertical_index = -1
+
+        if keys[pygame.K_UP]:
+            if not self.horizontal_transition and not self.player_selected:
+                self.sound.play_mode_select()
+                self.player_selected = True
+                self.vertical_transition = True
+                self.vertical_index = -1
+
+        if keys[pygame.K_LEFT]:
+            if not self.vertical_transition:
+                if self.player_selected and self.multiplayer:
+                    self.sound.play_mode_select()
+                    self.multiplayer = False
+                    self.horizontal_transition = True
+                    self.horizontal_index = -1
+                elif not self.player_selected and not self.story:
+                    self.sound.play_mode_select()
+                    self.story = True
+                    self.horizontal_transition = True
+                    self.horizontal_index = -1
+
+        if keys[pygame.K_RIGHT]:
+            if not self.vertical_transition:
+                if self.player_selected and not self.multiplayer:
+                    self.sound.play_mode_select()
+                    self.multiplayer = True
+                    self.horizontal_transition = True
+                    self.horizontal_index = -1
+                elif not self.player_selected and self.story:
+                    self.sound.play_mode_select()
+                    self.story = False
+                    self.horizontal_transition = True
+                    self.horizontal_index = -1
+
+    def update_menu(self):
+
+        self.is_player1_ready = True
+        self.is_story_mode = self.story
+
+        self.input()
+        if self.vertical_transition:
+            self.vertical_index += 1
+            if self.vertical_index == 5:
+                self.vertical_transition = False
+
+        if self.horizontal_transition:
+            self.horizontal_index += 1
+            if self.horizontal_index == 5:
+                self.horizontal_transition = False
+
+        h_alt_index = 5 - self.horizontal_index
+        v_alt_index = 5 - self.vertical_index
+
+        player_frame = self.multi_frames if self.multiplayer else self.single_frames
+
+        # if self.player_selected:
+        index = self.vertical_index if self.player_selected else v_alt_index
+        frame = player_frame[index]
+        frame_rect = frame.get_rect(center=(SCREEN_WIDTH * 0.5, SCREEN_HEIGHT - 120))
+        self.screen.blit(frame, frame_rect)
+
+        mode_frame = self.story_frames if self.story else self.survival_frames
+        # Mode Option
+        index = v_alt_index if self.player_selected else self.vertical_index
+        frame = mode_frame[index]
+        frame_rect = frame.get_rect(center=(SCREEN_WIDTH * 0.5, SCREEN_HEIGHT - 90))
+        self.screen.blit(frame, frame_rect)
+
+        info = 0
+        if self.multiplayer:
+            self.is_player2_ready = True
+            if self.story:
+                info = 1
+            else:
+                info = 3
+        else:
+            self.is_player2_ready = False
+            if self.story:
+                info = 0
+            else:
+                info = 2
+        frame = self.info_msg[info]
+        frame_rect = frame.get_rect(center=(SCREEN_WIDTH * 0.5, SCREEN_HEIGHT - 70))
+        self.screen.blit(frame, frame_rect)
+
+    def create_frames(self, text_msg):
+        frames = []
+        selected_color = ['red', 'white']
+        deselected_color = ['black', 'grey']
+
+        for i in range(6):
+            text_surf = pygame.Surface((300, 50), pygame.SRCALPHA)
+            text_surf.fill((0, 0, 0, 0))
+            font = pygame.font.Font('./assets/fonts/1.ttf', 17 + i)
+            color = selected_color if i > 0 else deselected_color
+
+            text = font.render(text_msg, False, color[0])
+            rect = text.get_rect(center=(150, 25))
+            text_surf.blit(text, rect)
+            text = font.render(text_msg, False, color[1])
+            rect = text.get_rect(center=(150 - 1, 25 - 2))
+            text_surf.blit(text, rect)
+            frames.append(text_surf)
+
+        return frames
+
+    def text(self, text_msg, size):
+
+        width = 500
+        text_surf = pygame.Surface((width, 50), pygame.SRCALPHA)
+        text_surf.fill((0, 0, 0, 0))
+
+        font = pygame.font.Font('./assets/fonts/4.ttf', size)
+        text = font.render(text_msg, False, 'black')
+        text_rect = text.get_rect(center=(width / 2, 25))
+        text_surf.blit(text, text_rect)
+        text = font.render(text_msg, False, 'white')
+        text_rect = text.get_rect(center=((width / 2) - 1, 25 - 1))
+        text_surf.blit(text, text_rect)
+
+        return text_surf
